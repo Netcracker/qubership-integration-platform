7508ac5b417b97ed2e39c7e5bf005fa001b1cf45
Sid775 <sviridov.id775@gmail.com>
2024-12-17 18:45:20 +0300

    Initial commit

---
+++ infrastructure/init-db/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE engine_qrtz_db;
+GRANT ALL PRIVILEGES ON DATABASE engine_qrtz_db TO postgres;
+
+CREATE DATABASE engine_checkpoints_db;
+GRANT ALL PRIVILEGES ON DATABASE engine_checkpoints_db TO postgres;
