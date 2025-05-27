Netcracker/qubership-integration-ui

feat: #19 Show Details for notifications

src/components/modal/ImportSessions.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, notification, UploadFile } from "antd";
+import { Button, Modal, UploadFile } from "antd";
 import { useModalContext } from "../../ModalContextProvider.tsx";
 import React, { useState } from "react";
 import { ExclamationCircleOutlined, InboxOutlined } from "@ant-design/icons";
@@ -6,6 +6,7 @@ import Dragger from "antd/es/upload/Dragger";
 import styles from "./ImportSessions.module.css";
 import { api } from "../../api/api.ts";
 import { Session } from "../../api/apiTypes.ts";
+import { useNotificationService } from "../../hooks/useNotificationService.tsx";
 
 type ImportSessionsProps = {
   onSuccess?: (sessions: Session[]) => void;
@@ -15,6 +16,7 @@ export const ImportSessions: React.FC<ImportSessionsProps> = ({ onSuccess }) => 
   const { closeContainingModal } = useModalContext();
   const [fileList, setFileList] = useState<UploadFile[]>([]);
   const [uploading, setUploading] = useState(false);
+  const notificationService = useNotificationService();
 
   const importSessions = async () => {
     setUploading(true);
@@ -23,10 +25,7 @@ export const ImportSessions: React.FC<ImportSessionsProps> = ({ onSuccess }) => 
       const sessions = await api.importSessions(files);
       onSuccess?.(sessions);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to import sessions",
-      });
+      notificationService.errorWithDetails("Failed to import sessions", error);
     } finally {
       setUploading(false);
     }

src/components/modal/SnapshotXml.tsx
@@ -1,9 +1,10 @@
-import { Modal, notification } from "antd";
+import { Modal } from "antd";
 import { useModalContext } from "../../ModalContextProvider.tsx";
 import React, { useEffect, useState } from "react";
 import { Snapshot } from "../../api/apiTypes.ts";
 import { api } from "../../api/api.ts";
 import { Editor } from "@monaco-editor/react";
+import { useNotificationService } from "../../hooks/useNotificationService.tsx";
 
 type SnapshotXmlViewProps = {
   snapshotId: string;
@@ -15,6 +16,7 @@ export const SnapshotXmlView: React.FC<SnapshotXmlViewProps> = ({
   const { closeContainingModal } = useModalContext();
   const [isLoading, setIsLoading] = useState(false);
   const [snapshot, setSnapshot] = useState<Snapshot>();
+  const notificationService = useNotificationService();
 
   useEffect(() => {
     getSnapshot(snapshotId);
@@ -25,10 +27,7 @@ export const SnapshotXmlView: React.FC<SnapshotXmlViewProps> = ({
     try {
       setSnapshot(await api.getSnapshot(snapshotId));
     } catch (err) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to get snapshot",
-      });
+      notificationService.errorWithDetails("Failed to get snapshot", err);
     } finally {
       setIsLoading(false);
     }

src/hooks/graph/useChainGraph.tsx
@@ -14,14 +14,15 @@ import {
 import { useCallback, useEffect } from "react";
 import { api } from "../../api/api.ts";
 import { Connection, Element } from "../../api/apiTypes.ts";
-import { notification } from "antd";
 import { useAutoLayout } from "./useAutoLayout.tsx";
+import { useNotificationService } from "../useNotificationService.tsx";
 
 export const useChainGraph = (chainId?: string) => {
   const { screenToFlowPosition } = useReactFlow();
 
   const [nodes, setNodes] = useNodesState<Node>([]);
   const [edges, setEdges] = useEdgesState<Edge>([]);
+  const notificationService = useNotificationService();
 
   const { autoLayout, elkDirectionControl } = useAutoLayout(
     nodes,
@@ -58,10 +59,7 @@ export const useChainGraph = (chainId?: string) => {
 
         autoLayout(newNodes, newEdges);
       } catch (error) {
-        notification.error({
-          message: "Request failed",
-          description: "Failed to load elements or connections",
-        });
+        notificationService.errorWithDetails("Failed to load elements or connections", error);
       }
     };
     fetchData();
@@ -85,10 +83,7 @@ export const useChainGraph = (chainId?: string) => {
         console.log(params);
         setEdges((eds) => addEdge(params, eds));
       } catch (error) {
-        notification.error({
-          message: "Request failed",
-          description: "Failed to create connection",
-        });
+        notificationService.errorWithDetails("Failed to create connection", error);
       }
     },
     [setEdges],
@@ -132,10 +127,7 @@ export const useChainGraph = (chainId?: string) => {
           setNodes((nodes) => nodes.concat(newNode));
         }
       } catch (error) {
-        notification.error({
-          message: "Request failed",
-          description: "Failed to create element",
-        });
+        notificationService.errorWithDetails("Failed to create element", error);
       }
     },
     [screenToFlowPosition],

src/hooks/useDeployments.tsx
@@ -1,11 +1,12 @@
 import { api } from "../api/api.ts";
 import { Deployment } from "../api/apiTypes.ts";
 import { useEffect, useState } from "react";
-import { notification } from "antd";
+import { useNotificationService } from "./useNotificationService.tsx";
 
 export const useDeployments = (chainId?: string) => {
   const [isLoading, setIsLoading] = useState(false);
   const [deployments, setDeployments] = useState<Deployment[]>();
+  const notificationService = useNotificationService();
 
   useEffect(() => {
     updateDeployments();
@@ -17,10 +18,7 @@ export const useDeployments = (chainId?: string) => {
       setIsLoading(true);
       return await api.getDeployments(chainId);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to load deployments",
-      });
+      notificationService.errorWithDetails("Failed to load deployments", error);
     } finally {
       setIsLoading(false);
     }

src/hooks/useDomains.tsx
@@ -1,11 +1,12 @@
 import { api } from "../api/api.ts";
 import { EngineDomain } from "../api/apiTypes.ts";
 import { useEffect, useState } from "react";
-import { notification } from "antd";
+import { useNotificationService } from "./useNotificationService.tsx";
 
 export const useDomains = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [domains, setDomains] = useState<EngineDomain[]>();
+  const notificationService = useNotificationService();
 
   useEffect(() => {
     getDomains();
@@ -17,10 +18,7 @@ export const useDomains = () => {
       const data = await api.getDomains();
       setDomains(data);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to load domains",
-      });
+      notificationService.errorWithDetails("Failed to load domains", error);
     } finally {
       setIsLoading(false);
     }

src/hooks/useSnapshots.tsx
@@ -1,11 +1,12 @@
 import { api } from "../api/api.ts";
 import { Snapshot } from "../api/apiTypes.ts";
 import { useEffect, useState } from "react";
-import { notification } from "antd";
+import { useNotificationService } from "./useNotificationService.tsx";
 
 export const useSnapshots = (chainId?: string) => {
   const [isLoading, setIsLoading] = useState(false);
   const [snapshots, setSnapshots] = useState<Snapshot[]>();
+  const notificationService = useNotificationService();
 
   useEffect(() => {
     if (!chainId) return;
@@ -18,10 +19,7 @@ export const useSnapshots = (chainId?: string) => {
       const data = await api.getSnapshots(chainId);
       setSnapshots(data);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to load snapshots",
-      });
+      notificationService.errorWithDetails("Failed to load snapshots", error);
     } finally {
       setIsLoading(false);
     }

src/pages/Deployments.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FloatButton, notification, Table, Tooltip } from "antd";
+import { FloatButton, Table, Tooltip } from "antd";
 import { useDeployments } from "../hooks/useDeployments.tsx";
 import { useParams } from "react-router";
 import { DeleteOutlined, PlusOutlined } from "@ant-design/icons";
@@ -12,12 +12,14 @@ import { useModalsContext } from "../Modals.tsx";
 import { DeploymentCreate } from "../components/modal/DeploymentCreate.tsx";
 import { api } from "../api/api.ts";
 import { LongActionButton } from "../components/LongActionButton.tsx";
+import { useNotificationService } from "../hooks/useNotificationService.tsx";
 
 export const Deployments: React.FC = () => {
   const { chainId } = useParams<{ chainId: string }>();
   const { isLoading, deployments, setDeployments } = useDeployments(chainId);
   const { snapshots } = useSnapshots(chainId);
   const { showModal } = useModalsContext();
+  const notificationService = useNotificationService();
 
   const columns: TableProps<Deployment>["columns"] = [
     {
@@ -83,10 +85,7 @@ export const Deployments: React.FC = () => {
       await api.deleteDeployment(deployment.id);
       setDeployments(deployments?.filter((d) => d.id !== deployment.id) ?? []);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to delete deployment",
-      });
+      notificationService.errorWithDetails("Failed to delete deployment", error);
     }
   };
 
@@ -96,10 +95,7 @@ export const Deployments: React.FC = () => {
       const deployment = await api.createDeployment(chainId, request);
       await onDeploymentCreated(deployment);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to create deployment",
-      });
+      notificationService.errorWithDetails("Failed to create deployment", error);
     }
   };
 

Netcracker/qubership-integration-platform

ui/src/components/modal/ImportSessions.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal, notification, UploadFile } from "antd";
+import { Button, Modal, UploadFile } from "antd";
 import { useModalContext } from "../../ModalContextProvider.tsx";
 import React, { useState } from "react";
 import { ExclamationCircleOutlined, InboxOutlined } from "@ant-design/icons";
@@ -6,6 +6,7 @@ import Dragger from "antd/es/upload/Dragger";
 import styles from "./ImportSessions.module.css";
 import { api } from "../../api/api.ts";
 import { Session } from "../../api/apiTypes.ts";
+import { useNotificationService } from "../../hooks/useNotificationService.tsx";
 
 type ImportSessionsProps = {
   onSuccess?: (sessions: Session[]) => void;
@@ -15,6 +16,7 @@ export const ImportSessions: React.FC<ImportSessionsProps> = ({ onSuccess }) => 
   const { closeContainingModal } = useModalContext();
   const [fileList, setFileList] = useState<UploadFile[]>([]);
   const [uploading, setUploading] = useState(false);
+  const notificationService = useNotificationService();
 
   const importSessions = async () => {
     setUploading(true);
@@ -23,10 +25,7 @@ export const ImportSessions: React.FC<ImportSessionsProps> = ({ onSuccess }) => 
       const sessions = await api.importSessions(files);
       onSuccess?.(sessions);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to import sessions",
-      });
+      notificationService.requestFailed("Failed to import sessions", error);
     } finally {
       setUploading(false);
     }

ui/src/components/modal/SnapshotXml.tsx
@@ -1,9 +1,10 @@
-import { Modal, notification } from "antd";
+import { Modal } from "antd";
 import { useModalContext } from "../../ModalContextProvider.tsx";
 import React, { useEffect, useState } from "react";
 import { Snapshot } from "../../api/apiTypes.ts";
 import { api } from "../../api/api.ts";
 import { Editor } from "@monaco-editor/react";
+import { useNotificationService } from "../../hooks/useNotificationService.tsx";
 
 type SnapshotXmlViewProps = {
   snapshotId: string;
@@ -15,6 +16,7 @@ export const SnapshotXmlView: React.FC<SnapshotXmlViewProps> = ({
   const { closeContainingModal } = useModalContext();
   const [isLoading, setIsLoading] = useState(false);
   const [snapshot, setSnapshot] = useState<Snapshot>();
+  const notificationService = useNotificationService();
 
   useEffect(() => {
     getSnapshot(snapshotId);
@@ -25,10 +27,7 @@ export const SnapshotXmlView: React.FC<SnapshotXmlViewProps> = ({
     try {
       setSnapshot(await api.getSnapshot(snapshotId));
     } catch (err) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to get snapshot",
-      });
+      notificationService.requestFailed("Failed to get snapshot", err);
     } finally {
       setIsLoading(false);
     }

ui/src/hooks/graph/useChainGraph.tsx
@@ -14,8 +14,8 @@ import {
 import { useCallback, useEffect, useState } from "react";
 import { api } from "../../api/api.ts";
 import { Connection, Element } from "../../api/apiTypes.ts";
-import { notification } from "antd";
 import { useAutoLayout } from "./useAutoLayout.tsx";
+import { useNotificationService } from "../useNotificationService.tsx";
 
 export const useChainGraph = (chainId?: string) => {
   const { screenToFlowPosition } = useReactFlow();
@@ -23,6 +23,7 @@ export const useChainGraph = (chainId?: string) => {
   const [nodes, setNodes] = useNodesState<Node>([]);
   const [edges, setEdges] = useEdgesState<Edge>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const notificationService = useNotificationService();
 
   const { autoLayout, elkDirectionControl } = useAutoLayout(
     nodes,
@@ -62,10 +63,7 @@ export const useChainGraph = (chainId?: string) => {
 
         autoLayout(newNodes, newEdges);
       } catch (error) {
-        notification.error({
-          message: "Request failed",
-          description: "Failed to load elements or connections",
-        });
+        notificationService.requestFailed("Failed to load elements or connections", error);
       } finally {
         setIsLoading(false);
       }
@@ -91,10 +89,7 @@ export const useChainGraph = (chainId?: string) => {
         console.log(params);
         setEdges((eds) => addEdge(params, eds));
       } catch (error) {
-        notification.error({
-          message: "Request failed",
-          description: "Failed to create connection",
-        });
+        notificationService.requestFailed("Failed to create connection", error);
       }
     },
     [setEdges],
@@ -138,10 +133,7 @@ export const useChainGraph = (chainId?: string) => {
           setNodes((nodes) => nodes.concat(newNode));
         }
       } catch (error) {
-        notification.error({
-          message: "Request failed",
-          description: "Failed to create element",
-        });
+        notificationService.requestFailed("Failed to create element", error);
       }
     },
     [screenToFlowPosition],

ui/src/hooks/useDeployments.tsx
@@ -1,11 +1,12 @@
 import { api } from "../api/api.ts";
 import { Deployment } from "../api/apiTypes.ts";
 import { useEffect, useState } from "react";
-import { notification } from "antd";
+import { useNotificationService } from "./useNotificationService.tsx";
 
 export const useDeployments = (chainId?: string) => {
   const [isLoading, setIsLoading] = useState(false);
   const [deployments, setDeployments] = useState<Deployment[]>();
+  const notificationService = useNotificationService();
 
   useEffect(() => {
     updateDeployments();
@@ -17,10 +18,7 @@ export const useDeployments = (chainId?: string) => {
       setIsLoading(true);
       return await api.getDeployments(chainId);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to load deployments",
-      });
+      notificationService.requestFailed("Failed to load deployments", error);
     } finally {
       setIsLoading(false);
     }

ui/src/hooks/useDomains.tsx
@@ -1,11 +1,12 @@
 import { api } from "../api/api.ts";
 import { EngineDomain } from "../api/apiTypes.ts";
 import { useEffect, useState } from "react";
-import { notification } from "antd";
+import { useNotificationService } from "./useNotificationService.tsx";
 
 export const useDomains = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [domains, setDomains] = useState<EngineDomain[]>();
+  const notificationService = useNotificationService();
 
   useEffect(() => {
     getDomains();
@@ -17,10 +18,7 @@ export const useDomains = () => {
       const data = await api.getDomains();
       setDomains(data);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to load domains",
-      });
+      notificationService.requestFailed("Failed to load domains", error);
     } finally {
       setIsLoading(false);
     }

ui/src/hooks/useSnapshots.tsx
@@ -1,11 +1,12 @@
 import { api } from "../api/api.ts";
 import { Snapshot } from "../api/apiTypes.ts";
 import { useEffect, useState } from "react";
-import { notification } from "antd";
+import { useNotificationService } from "./useNotificationService.tsx";
 
 export const useSnapshots = (chainId?: string) => {
   const [isLoading, setIsLoading] = useState(false);
   const [snapshots, setSnapshots] = useState<Snapshot[]>();
+  const notificationService = useNotificationService();
 
   useEffect(() => {
     if (!chainId) return;
@@ -18,10 +19,7 @@ export const useSnapshots = (chainId?: string) => {
       const data = await api.getSnapshots(chainId);
       setSnapshots(data);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to load snapshots",
-      });
+      notificationService.requestFailed("Failed to load snapshots", error);
     } finally {
       setIsLoading(false);
     }

ui/src/pages/Deployments.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FloatButton, notification, Table, Tooltip } from "antd";
+import { FloatButton, Table, Tooltip } from "antd";
 import { useDeployments } from "../hooks/useDeployments.tsx";
 import { useParams } from "react-router";
 import { DeleteOutlined, PlusOutlined } from "@ant-design/icons";
@@ -12,12 +12,14 @@ import { useModalsContext } from "../Modals.tsx";
 import { DeploymentCreate } from "../components/modal/DeploymentCreate.tsx";
 import { api } from "../api/api.ts";
 import { LongActionButton } from "../components/LongActionButton.tsx";
+import { useNotificationService } from "../hooks/useNotificationService.tsx";
 
 export const Deployments: React.FC = () => {
   const { chainId } = useParams<{ chainId: string }>();
   const { isLoading, deployments, setDeployments } = useDeployments(chainId);
   const { snapshots } = useSnapshots(chainId);
   const { showModal } = useModalsContext();
+  const notificationService = useNotificationService();
 
   const columns: TableProps<Deployment>["columns"] = [
     {
@@ -83,10 +85,7 @@ export const Deployments: React.FC = () => {
       await api.deleteDeployment(deployment.id);
       setDeployments(deployments?.filter((d) => d.id !== deployment.id) ?? []);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to delete deployment",
-      });
+      notificationService.requestFailed("Failed to delete deployment", error);
     }
   };
 
@@ -96,10 +95,7 @@ export const Deployments: React.FC = () => {
       const deployment = await api.createDeployment(chainId, request);
       await onDeploymentCreated(deployment);
     } catch (error) {
-      notification.error({
-        message: "Request failed",
-        description: "Failed to create deployment",
-      });
+      notificationService.requestFailed("Failed to create deployment", error);
     }
   };
 