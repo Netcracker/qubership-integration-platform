b8884b6fa8ea112b021b7417dfdfda668b60834d
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2026-05-21 15:48:00 +0300

    feat: implemented chain DSL resources suffix generator

--- runtime-catalog/src/main/java/org/qubership/integration/platform/runtime/catalog/cr/naming/strategies/IntegrationResourceNamingStrategy.java
+++ runtime-catalog/src/main/java/org/qubership/integration/platform/runtime/catalog/cr/naming/strategies/IntegrationResourceNamingStrategy.java
@@ -3,7 +3,9 @@ package org.qubership.integration.platform.runtime.catalog.cr.naming.strategies;
 import org.apache.commons.lang3.StringUtils;
 import org.qubership.integration.platform.runtime.catalog.cr.BuildInfo;
 import org.qubership.integration.platform.runtime.catalog.cr.ResourceBuildContext;
+import org.qubership.integration.platform.runtime.catalog.cr.naming.validation.K8sNameVerifier;
 import org.qubership.integration.platform.runtime.catalog.persistence.configs.entity.chain.Snapshot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,18 @@ import java.util.List;
 
 @Component("integrationResourceNamingStrategy")
 public class IntegrationResourceNamingStrategy extends K8sResourceNamingStrategy<ResourceBuildContext<List<Snapshot>>> {
-    @Value("${qip.cr.naming.prefix:}")
-    private String prefix;
+    private final String prefix;
+
+    @Autowired
+    public IntegrationResourceNamingStrategy(
+        K8sNameVerifier nameVerifier,
+
+        @Value("${qip.cr.naming.prefix:}")
+        String prefix
+    ) {
+        super(nameVerifier);
+        this.prefix = prefix;
+    }
 
     @Override
     protected String proposeName(ResourceBuildContext<List<Snapshot>> context) {
