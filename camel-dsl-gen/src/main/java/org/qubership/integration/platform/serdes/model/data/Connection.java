3521394c427e27474bbc9c9c6458a12e614cac72
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2026-06-04 14:53:40 +0300

    feat: added library for output DSL generation

---
+++ camel-dsl-gen/src/main/java/org/qubership/integration/platform/serdes/model/data/Connection.java
@@ -0,0 +1,7 @@
+package org.qubership.integration.platform.serdes.model.data;
+
+public interface Connection {
+    Element getFrom();
+
+    Element getTo();
+}
---
+++ camel-dsl-gen/src/main/java/org/qubership/integration/platform/serdes/model/io/DataFormatAwareReader.java
@@ -0,0 +1,4 @@
+package org.qubership.integration.platform.serdes.model.io;
+
+public interface DataFormatAwareReader<T> extends DataFormatAware, DataReader<T> {
+}
