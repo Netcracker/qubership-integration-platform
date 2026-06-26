19923418ce83c75c5024ccb70b56485cd154660f
Altair Kabdrakhmanov <altair200294@gmail.com>
2026-06-26 13:13:25 +0500

    fix: add test for chainDiffEditor.test.ts

--- vscode-extension/jest.config.cjs
+++ vscode-extension/jest.config.cjs
@@ -33,6 +33,7 @@ module.exports = {
         "<rootDir>/src/web/api-services/**/*.{ts,tsx}",
         "<rootDir>/src/web/response/**/*.{ts,tsx}",
         "<rootDir>/src/web/extension.ts",
+        "<rootDir>/src/web/chainDiffEditor.ts",
         "<rootDir>/src/web/editorViewTypes.ts",
         "<rootDir>/src/web/exportImageUtils.ts",
         "<rootDir>/src/web/exportImagesHandler.ts",
