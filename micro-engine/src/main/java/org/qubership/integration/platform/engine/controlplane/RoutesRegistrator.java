93333e8b7a579638d9cc38a469c9b570e53b3d8b
Anton Sushko <Sushco_anton@mail.ru>
2026-07-08 21:28:21 +0500

    fix: Add @Startup to RoutesRegistrator in order to have @PostConstruct method invoked

--- micro-engine/src/main/java/org/qubership/integration/platform/engine/controlplane/RoutesRegistrator.java
+++ micro-engine/src/main/java/org/qubership/integration/platform/engine/controlplane/RoutesRegistrator.java
@@ -5,6 +5,7 @@ import com.netcracker.cloud.routesregistration.common.gateway.route.RouteType;
 import com.netcracker.cloud.routesregistration.common.gateway.route.RoutesRestRegistrationProcessor;
 import io.quarkus.arc.Unremovable;
 import io.quarkus.arc.properties.IfBuildProperty;
+import io.quarkus.runtime.Startup;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -13,16 +14,21 @@ import org.qubership.integration.platform.engine.model.engine.EngineInfo;
 import org.qubership.integration.platform.engine.rest.v1.controller.CheckpointSessionController;
 import org.qubership.integration.platform.engine.rest.v1.controller.LiveExchangesController;
 import org.qubership.integration.platform.engine.rest.v1.controller.SessionController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.qubership.integration.platform.engine.rest.RestApiConstants.V1_ROUTE_PREFIX;
 
+@Startup
 @Unremovable
 @ApplicationScoped
 @IfBuildProperty(name = "qip.control-plane.routes.registration.enabled", stringValue = "true", enableIfMissing = true)
 public class RoutesRegistrator {
+    private static final Logger log = LoggerFactory.getLogger(RoutesRegistrator.class);
+
     private final RoutesRestRegistrationProcessor routesRestRegistrationProcessor;
     private final EngineInfo engineInfo;
 
@@ -40,15 +46,23 @@ public class RoutesRegistrator {
 
     @PostConstruct
     public void registerRoutes() {
+        log.info("[TEMP] registerRoutes() called, domain={}, publicPrefix={}", engineInfo.getDomain(), publicRoutePrefixV1);
         List<RouteEntry> routes = new ArrayList<>();
         routes.addAll(createRouteEntriesForAllGateways(SessionController.SESSIONS_PATH));
         routes.addAll(createRouteEntriesForAllGateways(CheckpointSessionController.CHECKPOINT_SESSION_PATH));
         routes.add(
-        new RouteEntry(publicRoutePrefixV1 + "/" + engineInfo.getDomain() + LiveExchangesController.LIVE_EXCHANGES_PATH,
-                 RouteType.PUBLIC
+            new RouteEntry(publicRoutePrefixV1 + "/" + engineInfo.getDomain() + LiveExchangesController.LIVE_EXCHANGES_PATH,
+                RouteType.PUBLIC
             )
         );
-        routesRestRegistrationProcessor.postRoutes(routes);
+        log.info("[TEMP] posting {} routes to control-plane: {}", routes.size(), routes);
+        try {
+            routesRestRegistrationProcessor.postRoutes(routes);
+            log.info("[TEMP] routes posted successfully");
+        } catch (Exception e) {
+            log.error("[TEMP] failed to post routes", e);
+            throw e;
+        }
     }
 
     private List<RouteEntry> createRouteEntriesForAllGateways(String apiPath) {
