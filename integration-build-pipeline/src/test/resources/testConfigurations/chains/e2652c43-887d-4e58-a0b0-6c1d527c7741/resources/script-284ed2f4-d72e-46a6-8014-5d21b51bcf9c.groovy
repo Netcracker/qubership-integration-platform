ab0bc16e713a14fe9017ada72342920527fcd63e
SukhochevAleksei <aleksei.sukhochev.qubership@gmail.com>
2026-09-09 14:26:22 +0300

    test: #168 Infrastructure for component testing + route elements tests

--- integration-build-pipeline/src/test/resources/testConfigurations/chains/e2652c43-887d-4e58-a0b0-6c1d527c7741/resources/script-284ed2f4-d72e-46a6-8014-5d21b51bcf9c.groovy
+++ integration-build-pipeline/src/test/resources/testConfigurations/chains/e2652c43-887d-4e58-a0b0-6c1d527c7741/resources/script-284ed2f4-d72e-46a6-8014-5d21b51bcf9c.groovy
@@ -3,13 +3,15 @@ def exceptionType = exchange.getProperty("exceptionType", String.class);
 if (exceptionType != "") {
 
   if (exceptionType == 'arithmetic'){
-    def a = 1 / 0;
+    def a = 1 / 0
+    exchange.setProperty('a', a)
   }
   else if (exceptionType == 'index'){
-    def arr = [1,2] as int[];
-    def i = arr[8];
+    def arr = [1,2] as int[]
+    def i = arr[8]
+    exchange.setProperty('i', i)
   }
   else{
-    throw new Exception("Other Exception");
+    throw new Exception("Other Exception")
   }
 }
