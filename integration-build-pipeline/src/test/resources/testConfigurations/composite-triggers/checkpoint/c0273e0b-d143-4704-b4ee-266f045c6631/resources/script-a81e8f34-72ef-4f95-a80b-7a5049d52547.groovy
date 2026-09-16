acbfcc71b77813828bbfdd5775fff4830c54c6fd
SukhochevAleksei <aleksei.sukhochev.qubership@gmail.com>
2026-09-16 13:57:33 +0300

    test: #169 extend route elements tests

--- integration-build-pipeline/src/test/resources/testConfigurations/composite-triggers/checkpoint/c0273e0b-d143-4704-b4ee-266f045c6631/resources/script-a81e8f34-72ef-4f95-a80b-7a5049d52547.groovy
+++ integration-build-pipeline/src/test/resources/testConfigurations/composite-triggers/checkpoint/c0273e0b-d143-4704-b4ee-266f045c6631/resources/script-a81e8f34-72ef-4f95-a80b-7a5049d52547.groovy
@@ -1,4 +1,4 @@
 def isCheckpoint = exchange.getProperty('originalSessionId', String) != null
-if (!isCheckpoint){
-  throw new RuntimeException('Checkpoint invocation failed before retry.')
+if (!isCheckpoint) {
+    throw new RuntimeException('Checkpoint invocation failed before retry.')
 }
--- integration-build-pipeline/src/test/resources/testConfigurations/routing/try-catch-finally-2/141e69f6-a106-47c9-ba45-090fdd7052a6/resources/d3c29565-da6b-435b-94bd-059af2cfcc46.element.script.cip.groovy
+++ integration-build-pipeline/src/test/resources/testConfigurations/routing/try-catch-finally-2/141e69f6-a106-47c9-ba45-090fdd7052a6/resources/d3c29565-da6b-435b-94bd-059af2cfcc46.element.script.cip.groovy
@@ -1,8 +1,8 @@
 def count = exchange.getProperty('finallyCount') ?: 0
 exchange.setProperty('finallyCount', count + 1)
 
-if(exchange.getProperty('finallyError') == true){
-  throw new Exception('finallyError')
+if (exchange.getProperty('finallyError') == true) {
+    throw new Exception('finallyError')
 }
 
 exchange.getMessage().setHeader('X-Finally-Step', true)
