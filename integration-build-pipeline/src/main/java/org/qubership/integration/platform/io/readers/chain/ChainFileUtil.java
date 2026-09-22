db53d740d7e9f2c25c16973d95a2acf2e828f624
Sergei Skuratovich <900852+SSNikolaevich@users.noreply.github.com>
2026-09-22 16:55:51 +0300

    fix: replaced hardcoded values with existed constants

--- integration-build-pipeline/src/main/java/org/qubership/integration/platform/io/readers/chain/ChainFileUtil.java
+++ integration-build-pipeline/src/main/java/org/qubership/integration/platform/io/readers/chain/ChainFileUtil.java
@@ -1,10 +1,13 @@
 package org.qubership.integration.platform.io.readers.chain;
 
+import static org.qubership.integration.platform.io.model.exportimport.ExportImportConstants.CHAIN_YAML_NAME_POSTFIX;
+import static org.qubership.integration.platform.io.model.exportimport.ExportImportConstants.CHAIN_YAML_NAME_PREFIX;
+
 public final class ChainFileUtil {
     private ChainFileUtil() {}
 
     public static boolean isChainFile(String fileName) {
         return (fileName.endsWith(".yaml") || fileName.endsWith(".yml"))
-            && (fileName.startsWith("chain-") || fileName.contains(".chain."));
+            && (fileName.startsWith(CHAIN_YAML_NAME_PREFIX) || fileName.contains(CHAIN_YAML_NAME_POSTFIX));
     }
 }
